1665ef6bcf564eb00a94272c0572caa34558d90f
aawadallah <aawadullah@eng.hu.edu.jo>
2018-11-29 15:57:21 +0200

    in

--- src/main/java/RegisterServlet.java
+++ src/main/java/RegisterServlet.java
@@ -5,13 +5,14 @@ public class RegisterServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         String userName = request.getParameter("username");
         String password = request.getParameter("pwd");
+        String encryptedPassword = StringUtil.toSHA256(password);
         String name= request.getParameter("name");
         String email = request.getParameter("email");
-        User newUser = new User.UserBuilder().setName(name).setUserName(userName).setEmail(email).setPassword(password).setDateOfRegister().build();
+        User newUser = new User.UserBuilder().setName(name).setUserName(userName).setEmail(email).setPassword(encryptedPassword).setDateOfRegister().build();
 
         RegisterDAOImpl regDAO = new RegisterDAOImpl();
         regDAO.registerUser(newUser);
-        response.sendRedirect("/JSP/SuccessRegister.jsp");
+        response.sendRedirect("/SuccessRegister.jsp");
 
 
     }
